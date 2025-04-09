81a75d8352541c948613d7955908f5e5b8dafcb7
Barbara Chacin <barbaraasucar@gmail.com>
2025-04-08 20:20:29 -0400

    deploy test #1

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SistemaDeDeudas.Models;
-using System.Threading.Tasks;
+using System;
 
 namespace SistemaDeDeudas.EFCore
 {
@@ -8,10 +8,26 @@ namespace SistemaDeDeudas.EFCore
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-
         }
 
         public DbSet<Cliente> Clientes { get; set; }
-        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
+                if (!string.IsNullOrEmpty(connectionString))
+                {
+                    optionsBuilder.UseNpgsql(connectionString);
+                }
+                else
+                {
+                    // Esta parte es opcional y puede ser útil para desarrollo local.
+                    // Reemplaza con tu cadena de conexión local si la necesitas.
+                    optionsBuilder.UseNpgsql("Server=localhost;Database=ClientesDb; User Id=postgres; Password=barbie08*.");
+                }
+            }
+        }
     }
 }
